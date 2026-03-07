a613279ddd5a79a10a0c2d5020854c53cc44feaa
MChapaev <magomed.chapaev1@gmail.com>
2026-03-07 15:10:35 +0300

    feat(Preference): add model

---
+++ WifeGift/WifeGift/DataAccess/Models/Preference.cs
@@ -0,0 +1,13 @@
+﻿namespace WifeGift.DataAccess.Models
+{
+    public class Preference : BaseEntity
+    {
+        public Guid UserDataId { get; set; }
+
+        required public UserData UserData { get; set; }
+
+        required public string Adjective { get; set; }
+
+        public double Weight { get; set; }
+    }
+}
