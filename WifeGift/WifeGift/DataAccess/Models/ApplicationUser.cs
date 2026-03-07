e16467b1244d10152896644411386242ccc0f2cd
MChapaev <magomed.chapaev1@gmail.com>
2026-03-07 15:10:51 +0300

    feat(ApplicationUser): add model

---
+++ WifeGift/WifeGift/DataAccess/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WifeGift.DataAccess.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual UserData UserData { get; set; }
+    }
+}
