be52ff6fdd089febc454eb9542462cf33b8b1fdb
MChapaev <magomed.chapaev1@gmail.com>
2026-03-07 19:31:19 +0300

    feat(dto): create API DTOs

---
+++ WifeGift/WifeGift/DataAccess/Models/Dto.cs
@@ -0,0 +1,19 @@
+﻿namespace WifeGift.DataAccess.Models
+{
+    public class Dto
+    {
+        public record PreferenceReadDto(Guid Id, string Adjective, double Weight);
+        public record PreferenceCreateDto(string Adjective, double Weight);
+        public record PreferenceUpdateDto(string Adjective, double Weight);
+
+        public record PreferenceWeightUpdateDto(double Weight);
+
+        public record UserDataReadDto(Guid Id, string UserId, List<PreferenceReadDto> Preferences);
+
+        public record UserProfileDto(
+            Guid Id,
+            string UserId,
+            DateTime LastLoginAt,
+            List<PreferenceReadDto> Preferences);
+    }
+}
