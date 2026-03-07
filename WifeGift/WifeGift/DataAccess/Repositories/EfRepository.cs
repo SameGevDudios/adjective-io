84d73b8d774e7fe467be02a04b69eaa5b7e80de0
MChapaev <magomed.chapaev1@gmail.com>
2026-03-07 14:14:08 +0300

    feat(EfRepository): add db repository

---
+++ WifeGift/WifeGift/DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WifeGift.DataAccess.Models;
+
+namespace WifeGift.DataAccess.Repositories
+{
+    public class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly DbContext _dbContext;
+
+        public EfRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var entities = await _dbContext.Set<T>().ToListAsync();
+
+            return entities;
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            var entity = await _dbContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> GetRangeByIdsAsync(List<Guid> ids)
+        {
+            var entities = await _dbContext.Set<T>().Where(x => ids.Contains(x.Id)).ToListAsync();
+
+            return entities;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ WifeGift/WifeGift/DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using WifeGift.DataAccess.Models;
+
+namespace WifeGift.DataAccess.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<T>> GetRangeByIdsAsync(List<Guid> ids);
+
+        Task AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
