c674748a0b7840d83ffee716b9b6e137777c8a4b
MChapaev <magomed.chapaev1@gmail.com>
2026-03-07 15:10:44 +0300

    feat(UserData): add model

---
+++ WifeGift/WifeGift/DataAccess/Models/UserData.cs
@@ -0,0 +1,11 @@
+﻿namespace WifeGift.DataAccess.Models
+{
+    public class UserData : BaseEntity
+    {
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public ICollection<Preference> Preferences { get; set; }
+    }
+}
