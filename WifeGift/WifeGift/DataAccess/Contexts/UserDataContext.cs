1a9a5d5ebf916e30a035dbc65d1f374e1cb02667
MChapaev <magomed.chapaev1@gmail.com>
2026-03-07 15:11:09 +0300

    feat(UserData): add db context

---
+++ WifeGift/WifeGift/DataAccess/Contexts/UserDataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WifeGift.DataAccess.Models;
+
+namespace WifeGift.DataAccess.Contexts
+{
+    public class UserDataContext
+    : DbContext
+    {
+        public UserDataContext(DbContextOptions<UserDataContext> options)
+        : base(options) { }
+
+        public DbSet<UserData> UserData { get; set; }
+        public DbSet<Preference> Preferences { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Preference>()
+                .HasOne(p => p.UserData)
+                .WithMany(u => u.Preferences)
+                .HasForeignKey(p => p.UserDataId);
+        }
+    }
+}
