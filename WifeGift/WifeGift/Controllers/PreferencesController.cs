998a7795e807cd84830b48ecf5af83b7c7a6b2c5
MChapaev <magomed.chapaev1@gmail.com>
2026-03-08 18:01:25 +0300

    refactor(PreferencesController): add explicit constructor

--- WifeGift/WifeGift/Controllers/PreferencesController.cs
+++ WifeGift/WifeGift/Controllers/PreferencesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WifeGift.Core.Configuration.PreferenceSettings;
 using WifeGift.Core.Extensions;
 using WifeGift.Core.Services.ProfileService;
 using static WifeGift.DataAccess.Models.Dto;
@@ -8,12 +9,19 @@ namespace WifeGift.Controllers
 {
     [Authorize]
     [Route("api/v1/preferences")]
-    public class PreferencesController(IProfileService profileService) : BaseApiController
+    public class PreferencesController : BaseApiController
     {
+        private readonly IProfileService _profileService;
+
+        public PreferencesController(IProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<PreferenceReadDto>>> GetAll()
         {
-            var allPreferences = await profileService.GetUserPreferencesAsync(CurrentUserId);
+            var allPreferences = await _profileService.GetUserPreferencesAsync(CurrentUserId);
 
             // Return only 70% of positive and 30% of negative adjectives
             var positivePreferences = allPreferences.Where(p => p.Weight >= 0).ToList();
@@ -33,10 +41,10 @@ namespace WifeGift.Controllers
 
         [HttpPatch("{id:guid}/increment")]
         public async Task<IActionResult> IncrementWeight(Guid id) =>
-            await profileService.IncreasePreferenceWeightAsync(CurrentUserId, id) ? Ok() : BadRequest();
+            await _profileService.IncreasePreferenceWeightAsync(CurrentUserId, id) ? Ok() : BadRequest();
 
         [HttpPatch("{id:guid}/decrement")]
         public async Task<IActionResult> Decrement(Guid id) =>
-            await profileService.DecreasePreferenceWeightAsync(CurrentUserId, id) ? Ok() : BadRequest();
+            await _profileService.DecreasePreferenceWeightAsync(CurrentUserId, id) ? Ok() : BadRequest();
     }
 }
