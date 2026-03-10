f1be6b21a2bee36377577971a8fa33f0e083ae04
MChapaev <magomed.chapaev1@gmail.com>
2026-03-10 18:13:23 +0300

    feat(ui_colors): add determined color collection

---
+++ WIfeGiftApp/lib/common/ui_colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+final class UiColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color background = Color(0xFFE0006E);
+  static const Color accent = Color(0xFFE83D92);
+  static const Color accentLight = Color(0xFFF074B1);
+  static const Color accentDark = Color(0xFFAE0062);
+  static const Color stunningAdjective = Color(0xFFFFD100);
+  static const Color positiveAdjective = Color(0xFF9F3ED5);
+  static const Color neutralAdjective = Color(0xFFE3E3E3);
+  static const Color negativeAdjective = Color(0xFFFF1800);
+  static const Color textPrimary = Color(0xFFAD66D5);
+  static const Color textSecondary = Color(0xFF727272);
+  static const Color textLight = Color(0xFFEFCCE0);
+  static const Color textDark = Color(0xFF484848);
+}
