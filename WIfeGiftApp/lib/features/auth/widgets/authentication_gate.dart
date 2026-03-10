3e0ae9d40054d62cbaa1cb491243dc2515731004
MChapaev <magomed.chapaev1@gmail.com>
2026-03-10 11:11:33 +0300

    feat(authentication): add authentication UI base

---
+++ WIfeGiftApp/lib/features/auth/widgets/authentication_gate.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wife_gift/features/auth/logic/auth_bloc.dart';
+
+class AuthenticationGate extends StatelessWidget {
+  const AuthenticationGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          if (state is AuthState$Loading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (state is AuthState$Auth) {
+            // return HomeScreen();
+            return const SizedBox.shrink();
+          }
+
+          if (state is AuthState$Unauth) {
+            // return AuthenticationScreen();
+            return const SizedBox.shrink();
+          }
+
+          // return ErrorModal();
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+  }
+}
