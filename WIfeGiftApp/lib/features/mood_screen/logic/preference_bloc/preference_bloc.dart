f005fa0614e17631230d0adf9585d0313e92d924
MChapaev <magomed.chapaev1@gmail.com>
2026-03-11 16:17:33 +0300

    feat(preference_bloc): add bloc

---
+++ WIfeGiftApp/lib/features/mood_screen/logic/preference_bloc/preference_bloc.dart
@@ -0,0 +1,61 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wife_gift/common/ui_colors.dart';
+import 'package:wife_gift/features/mood_screen/data/models/Adjective.dart';
+import 'package:wife_gift/features/mood_screen/data/models/preference.dart';
+import 'package:wife_gift/features/mood_screen/data/repositories/preference_repository.dart';
+
+part 'preference_event.dart';
+part 'preference_state.dart';
+
+class PreferenceBloc extends Bloc<PreferenceEvent, PreferenceState> {
+  final PreferenceRepository _repository;
+
+  PreferenceBloc(PreferenceRepository repository)
+    : _repository = repository,
+      super(PreferenceState$Initial()) {
+    on<PreferenceEvent$PreferencesRequested>(_onPreferencesRequested);
+    on<PreferenceEvent$AddRequested>(_onPreferenceAddRequested);
+  }
+
+  Future<void> _onPreferencesRequested(
+    PreferenceEvent$PreferencesRequested event,
+    Emitter<PreferenceState> emit,
+  ) async {
+    emit(PreferenceState$Loading());
+
+    try {
+      final preferences = await _repository.getAllPreferences();
+
+      final adjectives = preferences.map(Adjective.fromPreference).toList();
+
+      emit(PreferenceState$Success(adjectives: adjectives));
+    } catch (e) {
+      emit(PreferenceState$Error(e.toString()));
+    }
+  }
+
+  Future<void> _onPreferenceAddRequested(
+    PreferenceEvent$AddRequested event,
+    Emitter<PreferenceState> emit,
+  ) async {
+    if (state is! PreferenceState$Success) {
+      return;
+    }
+
+    emit(PreferenceState$Loading());
+
+    try {
+      await _repository.addPreferences(event.preferences);
+      final success = state as PreferenceState$Success;
+
+      emit(
+        success.copyWith(
+          success.adjectives + event.preferences.map(Adjective.fromPreference).toList(),
+        ),
+      );
+    } catch (e) {
+      emit(PreferenceState$Error(e.toString()));
+    }
+  }
+}
