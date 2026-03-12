39be0708abf09df43f69c8773db9f52f8519844e
MChapaev <magomed.chapaev1@gmail.com>
2026-03-12 18:18:29 +0300

    refactor(prefix_widget): remove excessive code

--- WIfeGiftApp/lib/features/mood_screen/widgets/prefix_widget.dart
+++ WIfeGiftApp/lib/features/mood_screen/widgets/prefix_widget.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wife_gift/common/ui_colors.dart';
 import 'package:wife_gift/features/mood_screen/data/models/prefix.dart';
