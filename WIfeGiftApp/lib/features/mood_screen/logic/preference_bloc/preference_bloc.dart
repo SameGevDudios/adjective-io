3cf99a6e263be84368b15df73633951711034801
MChapaev <magomed.chapaev1@gmail.com>
2026-03-12 21:40:37 +0300

    refactor(adjective): rename

--- WIfeGiftApp/lib/features/mood_screen/logic/preference_bloc/preference_bloc.dart
+++ WIfeGiftApp/lib/features/mood_screen/logic/preference_bloc/preference_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:wife_gift/common/ui_colors.dart';
-import 'package:wife_gift/features/mood_screen/data/models/Adjective.dart';
+import 'package:wife_gift/features/mood_screen/data/models/adjective.dart';
 import 'package:wife_gift/features/mood_screen/data/models/preference.dart';
 import 'package:wife_gift/features/mood_screen/data/repositories/preference_repository.dart';
 
