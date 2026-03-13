c81cf9b1b4c0f83e72137eb6a85c3392c6ba6eec
MChapaev <magomed.chapaev1@gmail.com>
2026-03-13 09:40:11 +0300

    feat(link_label): add text color

--- WIfeGiftApp/lib/common/widgets/buttons/link_label.dart
+++ WIfeGiftApp/lib/common/widgets/buttons/link_label.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 
 class LinkLabel extends StatelessWidget {
   final String message;
+  final Color? color;
   final VoidCallback onTap;
 
-  const LinkLabel({required this.message, required this.onTap, super.key});
+  const LinkLabel({required this.message, this.color, required this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      child: Text(message, style: TextStyle(decoration: TextDecoration.underline)),
+      child: Text(message, style: TextStyle(decoration: TextDecoration.underline, color: color)),
     );
   }
 }
