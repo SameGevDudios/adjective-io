efa51915c6292af18bbd1a24afc8cfa70610f85d
MChapaev <magomed.chapaev1@gmail.com>
2026-03-18 09:11:45 +0300

    feat(link_label): add style settings

--- WIfeGiftApp/lib/common/widgets/buttons/link_label.dart
+++ WIfeGiftApp/lib/common/widgets/buttons/link_label.dart
@@ -4,14 +4,30 @@ class LinkLabel extends StatelessWidget {
   final String message;
   final Color? color;
   final VoidCallback onTap;
+  final TextDecoration? decoration;
+  final FontWeight? fontWeight;
 
-  const LinkLabel({required this.message, this.color, required this.onTap, super.key});
+  const LinkLabel({
+    required this.message,
+    this.color,
+    required this.onTap,
+    this.decoration,
+    this.fontWeight,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      child: Text(message, style: TextStyle(decoration: TextDecoration.underline, color: color)),
+      child: Text(
+        message,
+        style: TextStyle(
+          decoration: decoration ?? TextDecoration.underline,
+          color: color,
+          fontWeight: fontWeight,
+        ),
+      ),
     );
   }
 }
